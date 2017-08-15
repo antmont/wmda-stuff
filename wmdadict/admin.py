4a28821ef286c42430a868fa5b919eb711aae39d
Anthony Montague <antmontalt@me.com>
2017-08-15 20:28:16 +1000

    Add dictionary fields to db

--- wmdadict/admin.py
+++ wmdadict/admin.py
@@ -8,8 +8,14 @@ class FormFieldsAdmin(admin.ModelAdmin):
 class DataFamilyAdmin(admin.ModelAdmin):
     verbose_name_plural = 'Data families'
 
+class DictionaryFieldAdmin(admin.ModelAdmin):
+    list_display = ('label', 'field_type', 'owner_family')
+    search_fields = ('label', 'description_short', 'description_long')
+    list_filter = ('owner_family', 'user_families')
+#    filter_horizontal = ('user_families',)
+
 admin.site.register(DataFamily, DataFamilyAdmin)
-admin.site.register(DictionaryField)
+admin.site.register(DictionaryField, DictionaryFieldAdmin)
 admin.site.register(EmdisMessage)
 admin.site.register(EmdisField)
 admin.site.register(BmdwField)
