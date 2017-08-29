b2a3088c28e2a2469c6952516e6749bc61a97c17
antmont <antmontalt@me.com>
2017-08-29 13:05:17 +1000

    Added invoke deploy

---
+++ tasks.py
@@ -0,0 +1,14 @@
+from invoke import task
+
+@task
+def deploy(ctx):
+    """Push to Heroku and runs any ancillary tasks necessary."""
+    print("Pushing latest changes to Heroku...")
+    ctx.run('pip freeze > requirements.txt')
+    ctx.run('git add .')
+    comment = input("Enter commit message: ")
+    ctx.run('git commit -m "%s"' % comment)
+    ctx.run("git push heroku master")
+    ctx.run("heroku run python manage.py syncdb")
+    ctx.run("heroku run python manage.py migrate")
+    ctx.run("heroku run python manage.py compress")
