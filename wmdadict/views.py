7b08f1daf1cc4a160d389debfe7d311d9b595323
Unknown <antmontalt@me.com>
2017-09-19 08:43:42 +1000

    Create context-driven collapsable panels for filters

--- wmdadict/views.py
+++ wmdadict/views.py
@@ -14,7 +14,14 @@ from .filters import BmdwFieldFilter, WmdaFormFilter
 def dictionary_list(request):
     dictionaryfields = DictionaryField.objects.all()
     field_filter = DictionaryFilter(request.GET, queryset=dictionaryfields)
-    return render(request, 'wmdadict/dictionary_list.html', {'dict_fields': field_filter})
+    if any(v != '' for v in request.GET.dict().values()):
+        filter_active = True
+    else:
+        filter_active = False
+    filter_context = {'filter_active': filter_active}
+    return render(request, 'wmdadict/dictionary_list.html',
+                                {'dict_fields': field_filter,
+                                 'filter_context': filter_context})
 
 def dictionary_detail(request, field_id):
     dictionaryfield = get_object_or_404(DictionaryField, pk=field_id)
@@ -26,7 +33,16 @@ def dictionary_detail(request, field_id):
 def emdis_list(request):
     emdis_fields = EmdisField.objects.all()
     field_filter = EmdisFieldFilter(request.GET, queryset=emdis_fields)
-    return render(request, 'wmdadict/emdis_list.html', {'emdis_fields': field_filter})
+    if any(v != '' for v in request.GET.dict().values()):
+        filter_active = True
+    else:
+        filter_active = False
+    filter_context = {'filter_active': filter_active}
+
+    return render(request,
+                  'wmdadict/emdis_list.html',
+                  {'emdis_fields': field_filter,
+                  'filter_context': filter_context})
 
 def emdis_detail(request, field_id):
     emdisfield = get_object_or_404(EmdisField, pk=field_id)
@@ -36,7 +52,15 @@ def emdis_detail(request, field_id):
 def emdis_msg_list(request):
     msg_fields = EmdisMessage.objects.all()
     msg_filter = EmdisMessageFilter(request.GET, queryset=msg_fields)
-    return render(request, 'wmdadict/emdis_msg_list.html', {'msg_filter': msg_filter})
+    if any(v != '' for v in request.GET.dict().values()):
+        filter_active = True
+    else:
+        filter_active = False
+    filter_context = {'filter_active': filter_active}
+    return render(request,
+                 'wmdadict/emdis_msg_list.html',
+                 {'msg_filter': msg_filter,
+                 'filter_context': filter_context})
 
 def emdis_msg_detail(request, msg_id):
     emsg = get_object_or_404(EmdisMessage, pk=msg_id)
@@ -49,7 +73,15 @@ def emdis_msg_detail(request, msg_id):
 def bmdw_list(request):
     bmdwfields = BmdwField.objects.all()
     bmdwfilter = BmdwFieldFilter(request.GET, queryset=bmdwfields)
-    return render(request, 'wmdadict/bmdw_list.html', {'bmdwfilter': bmdwfilter})
+    if any(v != '' for v in request.GET.dict().values()):
+        filter_active = True
+    else:
+        filter_active = False
+    filter_context = {'filter_active': filter_active}
+    return render(request,
+                  'wmdadict/bmdw_list.html',
+                  {'bmdwfilter': bmdwfilter,
+                  'filter_context': filter_context})
 
 def bmdw_detail(request, field_id):
     bmdwfield = get_object_or_404(BmdwField, pk=field_id)
@@ -60,7 +92,15 @@ def bmdw_detail(request, field_id):
 def form_list(request):
     wmdaforms = WmdaForm.objects.all()
     formfilter = WmdaFormFilter(request.GET, queryset=wmdaforms)
-    return render(request, 'wmdadict/form_list.html', {'formfilter': formfilter})
+    if any(v != '' for v in request.GET.dict().values()):
+        filter_active = True
+    else:
+        filter_active = False
+    filter_context = {'filter_active': filter_active}
+    return render(request,
+                  'wmdadict/form_list.html',
+                  {'formfilter': formfilter,
+                   'filter_context': filter_context})
 
 def form_detail(request, field_id):
     wmdaform = get_object_or_404(WmdaForm, pk=field_id)
