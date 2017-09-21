24498252cfc57771ce26171cbf19ac8463c570de
Anthony Montague <antmontalt@me.com>
2017-09-21 20:20:07 +1000

    Update install documentation and requirements

--- wmdasite/static/js/custom.js
+++ wmdasite/static/js/custom.js
@@ -17,3 +17,15 @@ $(document).on('click', '.panel-heading span.clickable', function(e){
 		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
 	}
 })
+
+/*** REVISED VERSION TO TRY ***/
+/* Manual code to collapse/un-collapse blocks with alternating chevrons
+   Used with filters in list pages */
+$(document).ready(function(){
+  $("#demo").on("hide.bs.collapse", function(){
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open');
+  });
+  $("#demo").on("show.bs.collapse", function(){
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Close');
+  });
+});
