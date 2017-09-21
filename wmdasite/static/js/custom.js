10747d778ad3ddbd96056bb5aa5a691733faa9fe
Anthony Montague <antmontalt@me.com>
2017-09-21 22:20:16 +1000

    Change filter collapse to use Bootstrap and (custom js onlyfor chevrons)

--- wmdasite/static/js/custom.js
+++ wmdasite/static/js/custom.js
@@ -1,31 +1,17 @@
-/* Enable Bootstrap popovers */
 $(document).ready(function(){
+    /* Enable Bootstrap popovers */
     $('[data-toggle="popover"]').popover();
-});
 
-/* Manual code to collapse/un-collapse blocks with alternating chevrons
-   Used with filters in list pages */
-$(document).on('click', '.panel-heading span.clickable', function(e){
-   var $this = $(this);
-	if(!$this.hasClass('panel-collapsed')) {
-		$this.parents('.panel').find('.panel-body').slideUp();
-		$this.addClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-	} else {
-		$this.parents('.panel').find('.panel-body').slideDown();
-		$this.removeClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-	}
-})
+    /* Update chevrons on filter panel-heading to shown up/down
+       as filter panel opens/closes
+       Uses filter-collapse for the Bootstrap collapse ID
+       Uses filter-label as a custom ID on the span that holds the chevron
+     */
+    $("#filter-collapse").on("hide.bs.collapse", function(){
+      $("#filter-label").html('<i class="glyphicon glyphicon-chevron-down"></i>');
+    });
+    $("#filter-collapse").on("show.bs.collapse", function(){
+      $("#filter-label").html('<i class="glyphicon glyphicon-chevron-up"></i>');
+    });
 
-/*** REVISED VERSION TO TRY ***/
-/* Manual code to collapse/un-collapse blocks with alternating chevrons
-   Used with filters in list pages */
-$(document).ready(function(){
-  $("#demo").on("hide.bs.collapse", function(){
-    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open');
-  });
-  $("#demo").on("show.bs.collapse", function(){
-    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Close');
-  });
 });
